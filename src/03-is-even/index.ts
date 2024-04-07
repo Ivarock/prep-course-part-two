85706f671aa93305348f6759d486cc352ac58848
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-07 13:09:57 +0000

    used bitwise AND operator

--- src/03-is-even/index.ts
+++ src/03-is-even/index.ts
@@ -9,6 +9,9 @@
  * isEven(3) === false
  */
 
-function isEven(n: number) {}
+function isEven(n: number) {
+    // Using bitwise & operator to see if least significant bit is 0: (0 - even number(true), 1 - odd number(false))
+    return (n & 1) === 0;
+}
 
 export { isEven };
