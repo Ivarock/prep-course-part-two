3307970675144003715e8fe6309314142d4b99f5
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-07 10:11:31 +0000

    wrote a function

--- src/00-acronym/index.ts
+++ src/00-acronym/index.ts
@@ -6,6 +6,10 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {}
+function parse(input: string): string {
+    const lettersOnly = input.match(/[a-zA-Z']+/g) || [];
+    const acronym = lettersOnly.map(word => word.charAt(0).toUpperCase());
+    return acronym.join('');
+}
 
 export { parse };
