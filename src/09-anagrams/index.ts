1712b180e3444291a0c0e9e6cfe0094b169aeaa9
Ivars Sluka <129661878+Ivarock@users.noreply.github.com>
2024-04-11 10:02:13 +0000

    used regEx to to remove all whitespace and punctuation, sorted strings and checked if they are equal

--- src/09-anagrams/index.ts
+++ src/09-anagrams/index.ts
@@ -12,6 +12,18 @@
  * anagrams('Hi there', 'Bye there') === false
  */
 
-function anagrams(stringA: string, stringB: string) {}
+function anagrams(stringA: string, stringB: string): boolean {
+  // Remove whitespace and punctuation, and convert input strings to lowercase
+  let str1 = stringA.replace(/[\W_]/g, "").toLowerCase();
+  let str2 = stringB.replace(/[\W_]/g, "").toLowerCase();
+  // Check if both strings are the same length
+  if (str1.length !== str2.length) {
+    return false;
+  }
+  // If strings are the same length we sort them and check if they are equal and return boolean
+  str1 = str1.split("").sort().join("");
+  str2 = str2.split("").sort().join("");
+  return str1 === str2;
+}
 
 export { anagrams };
